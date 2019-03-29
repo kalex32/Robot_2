fd8c4925a48e0fe30442d8723881290c4abec0da
Olexiy Koval <kalex32@ukr.net>
2019-03-29 02:56:26 +0200

    Order of parameters

--- src/main/java/impl/ModelT1000.java
+++ src/main/java/impl/ModelT1000.java
@@ -10,6 +10,10 @@ public class ModelT1000 implements Robot {
     private Head head;
     private Leg leg;
 
+    private String color;
+    private int year;
+    private boolean soundEnabled;
+
     public ModelT1000() {
     }
 
@@ -20,11 +24,30 @@ public class ModelT1000 implements Robot {
         this.leg = leg;
     }
 
+    ModelT1000(Hand hand, Head head, Leg leg, String color, int year, boolean soundEnabled) {
+        super();
+        this.hand = hand;
+        this.head = head;
+        this.leg = leg;
+        this.color = color;
+        this.year = year;
+        this.soundEnabled = soundEnabled;
+    }
+
+    ModelT1000(String color, int year, boolean soundEnabled) {
+        super();
+        this.color = color;
+        this.year = year;
+        this.soundEnabled = soundEnabled;
+    }
 
     public void action() {
         head.calc();
         hand.catchSomething();
         leg.go();
+        System.out.println("color: " + color);
+        System.out.println("year: " + year);
+        System.out.println("can play sound: " + soundEnabled);
     }
 
 
@@ -50,6 +73,30 @@ public class ModelT1000 implements Robot {
 
     public void setLeg(Leg leg) {
         this.leg = leg;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public boolean isSoundEnabled() {
+        return soundEnabled;
+    }
+
+    public void setSoundEnabled(boolean soundEnabled) {
+        this.soundEnabled = soundEnabled;
     }
 
     public void dance() {
